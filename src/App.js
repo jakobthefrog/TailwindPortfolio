a59c3cac0ab24788f56f9d5297d6c88860a456fc
JakobLegion <admin@jakobandersson.com>
2022-03-03 18:55:52 -0700

    tailwind test component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import TestComponent from './components/TestComponent';
 
 function App() {
   return (
-    <div className="App">
-      <h2 className='text-3xl font-bold underline'>Test</h2>
+    <div className="">
+      <TestComponent />
     </div>
   );
 }

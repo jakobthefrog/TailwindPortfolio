a08eb5edcd2963d9cfaf4e8e40849aae6ecd267d
JakobLegion <admin@jakobandersson.com>
2022-03-04 17:04:59 -0700

    fancy

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
+import IntroSection from './components/IntroSection';
 import TestComponent from './components/TestComponent';
 
 function App() {
-  return (
-    <div className="">
-      <TestComponent />
-    </div>
-  );
+	return (
+		<div className="">
+			<TestComponent />
+			<div className="container relative z-30 mx-auto">
+				<IntroSection />
+				<h3>Test</h3>
+			</div>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/IntroSection.js
@@ -0,0 +1,40 @@
+import ReactTypingEffect from 'react-typing-effect';
+
+const IntroSection = () => {
+	return (
+		<main className={`flex-col flex h-screen`} id="home">
+			<div className="container relative flex flex-col items-start justify-center flex-grow px-0 mx-auto md:px-20 lg:px-24 section">
+				<div className="w-full">
+					<span className="text-2xl font-semibold text-brand">
+						Hi! My name is
+					</span>
+					<h1 className="mb-4 text-5xl md:text-7xl">
+						Jakob Andersson
+					</h1>
+					<h2 className="mb-4 text-3xl md:text-4xl text-light">
+						<ReactTypingEffect
+							typingDelay={200}
+							speed={30}
+							eraseSpeed={30}
+							eraseDelay={1500}
+							text={[
+								`UI / UX Designer`,
+								`Front-End Developer`,
+								`Coffee Enthusiast`,
+								`Hobby Mechanic`,
+							]}
+						/>
+					</h2>
+					<p className="w-4/5 text-xl md:w-full">
+						I make products that people love to use!
+					</p>
+					{/* <button className="mt-4 btn-brand btn-lg">
+                    See my Work
+                </button> */}
+				</div>
+			</div>
+		</main>
+	);
+};
+
+export default IntroSection;
